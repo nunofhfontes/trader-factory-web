5a8b7e3e81853314310d3c93dbc5f8f28bc6adcc
nunofontes <nunof.h.fontes@gmail.com>
2021-04-28 23:02:14 +0100

    login function on authentication-service

--- trader-factory-web/src/_services.js/authentication.service.js
+++ trader-factory-web/src/_services.js/authentication.service.js
@@ -6,3 +6,21 @@ export const authenticationService = {
     currentUser: currentUserSubject.asObservable(),
     get currentUserValue () { return currentUserSubject.value }
 };
+
+function login(username, password) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+    };
+
+    return fetch(`${config.apiUrl}/users/authenticate`, requestOptions)
+        .then(handleResponse)
+        .then(user => {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            currentUserSubject.next(user);
+
+            return user;
+        });
+}
